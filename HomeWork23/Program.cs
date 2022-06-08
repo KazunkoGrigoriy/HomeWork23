76ad76eed3d65b9b0c9f3e289ef76c70b472539c
Grigoriy Kazunko <gkazunko@mail.ru>
2022-06-08 12:25:00 +0300

    Внесены изменения

--- HomeWork23/Program.cs
+++ HomeWork23/Program.cs
@@ -7,50 +7,60 @@ namespace HomeWork23
         public static double ToDouble(string str)
         {
             double chislo = 0;
-            int i = 0;
-            int k = 0;
-            int j = 0;
-            bool f = false;
-            for(i = 0; i < str.Length; i++)
+            int i = 0; //вспомогательный индекс для просмотра элементов строки в циклах
+            int namberOfDelimiters = 0; //количество точек и запятых в строке
+            int numberOfDecimal = 0; //количество цифр дробной части
+            bool negativeNumber = false; //флаг для проверки наличия знака "-" в строке, false - положительное число, true - отрицательное
+            
+            //Проверка наличия разделителя (точки или запятой) в строке
+            while(i < str.Length && namberOfDelimiters <= 1)
             {
-                if(str[i] == '.') k++;
-                if (k == 1) j++;
+                if(str[i] == '.' || str[i] == ',') namberOfDelimiters++; //количество разделителей в строке
+                if (namberOfDelimiters == 1 && str[i] != '.' && str[i] != ',') numberOfDecimal++; //количество цифр дробной части
+                    i++;
             }
 
-            if (k > 1) return 0;
+            //Если в строке разделителей больше одного - выход из метода, возврат 0
+            if (namberOfDelimiters > 1)
+            {
+                Console.WriteLine("Число некорректно");
+                return 0;
+            }
 
-            i = 0;
+            i = 0; 
+            
+            //Проверка наличия знака "-" в начале строки
             if (str[0] == '-')
             {
-                f = true;
+                negativeNumber = true; //число отрицательное
                 i = 1;
             }
+            
+            //При наличии знака "+" в начале строки - переход к следующему элементу
             if (str[0] == '+') i = 1;
 
-            for (i = 0; i < str.Length; i++)
+            //Цикл преобразования элементов строки в цифры
+            while(i < str.Length)
             {
-                if (str[i] >= 0x30 && str[i] <= 0x39)
+                if (str[i] >= 0x30 && str[i] <= 0x39) //сравнение сомвола с его кодом: 0х30 : 0 ... 0х39 : 9
                 {
                     chislo = chislo + (str[i] & 0x0F);
                     chislo = chislo * 10;
                 }
                 else
-                if (str[i] != '.')
                 {
-                    Console.WriteLine("Строка содержит символы отличные от цифр и точки");
-                    return 0;
+                    if (str[i] != '.' && str[i] != ',') //Если в строке есть символы отличные от цифр или разделителей - выход из цикла, возврат 0
+                    {
+                        Console.WriteLine("Число некорректно");
+                        return 0;
+                    }
                 }
+                i++;
             }
-            if (i == str.Length)
-            {
-                if (f) chislo = (-1) * chislo;
-                Console.WriteLine(chislo * (Math.Pow(10, (-1)*(j-1)))/10);
-                return chislo * (Math.Pow(10, (-1) * (j - 1))) / 10;
-            }
-            else
-            {
-                return 0;
-            }
+            
+            if (negativeNumber) chislo = (-1) * chislo; //Если в начале строки "-" - умножение числа на "-1"
+            Console.WriteLine(chislo * (Math.Pow(10, (-1)*numberOfDecimal)) / 10); //Вывод числа
+            return chislo * (Math.Pow(10, (-1)*numberOfDecimal) / 10); //Возврат числа
         }
         
         static void Main(string[] args)
